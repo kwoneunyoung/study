4e8160781f8e70f2b9e76985dcd9b80281b200ec
kwoneunyoung <dmsdud4972@naver.com>
2020-12-30 18:42:13 +0900

    1

---
+++ vue.js/doitVue/vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+var appHeader = {
+    template : '<div>header</div>',
+    methods : {
+        addNum : function() {
+
+        }
+    }
+}
+
+<template>
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    methods : {
+        addNum : function() {
+            
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
