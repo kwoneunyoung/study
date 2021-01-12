2d1d703715b058da3f3df98717fc9439b5a68c13
young <dmsdud4972@naver.com>
2021-01-12 16:32:49 +0900

    1

--- java/test 2/src/Element.java
+++ java/test 2/src/Element.java
@@ -1,11 +1,13 @@
 public class Element {
 
-    public String title; //title에 string 타입 자료 저장
-    Element link; // link에는 다음 엘리먼트에 대한 정보가 저장 될 것이다
+  
+   
     
 
     Element(String title) {
-        
         this.title = title;
     }
+    String title;
+    Element front = null;
+    Element back = null;
 }
--- java/test 2/src/TestList.java
+++ java/test 2/src/TestList.java
@@ -1,23 +1,58 @@
 public class TestList {
     Element head;
+    Element rear;
+    int size=0;
 
-    TestList() {
-        head = null;
+    private class Element {
+        private Object title;
+        private Element next;
+        public Element(Object input) {
+            this.title = input;
+            this.next = null;
+        }
+        public String toString() {
+            return String.valueOf(this.title);
+        }
     }
 
-    public boolean isEmpty() { //현재 비어있는지 확인하는 메소드
-        return this.head == null;
+    public void addFirst(Object input) {
+        Element newElement = new Element(input);
+        newElement.next = head;
+        head=newElement;
+        size++;
+
+        if(head.next == null ) {
+            rear = head;
+        }
+    }
+    
+    public void addLast(Object input) {
+        Element newElement = new Element(input);
+        if(size == 0) {
+            addFirst(input); 
+        } else {
+            rear.next = newElement;
+            rear = newElement;
+            size++;
+        }
     }
 
-    public void insertFirst(String title) {
+
+    void firstElement(String title) {
         Element newElement = new Element(title);
+        head = newElement;
+        rear = newElement;
+        size++;
+        System.out.println("firstElement(String title) 실행");
     }
 
     public void addElement(Element element) {
 
     }
     
-    public void removeElement(Element element) {
+    public void removeElement(Element element) { //전체 element 삭제
 
     }
+
+   
 }
