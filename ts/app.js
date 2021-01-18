e77791e8455976d823bf5175371b4ca649bb5a6b
young <dmsdud4972@naver.com>
2021-01-18 15:37:35 +0900

    1

--- ts/app.js
+++ ts/app.js
@@ -1,9 +1,1 @@
 "use strict";
-var sendGreeting = function (message, userName) {
-    if (message === void 0) { message = 'hello'; }
-    if (userName === void 0) { userName = 'there'; }
-    return console.log(message + ", " + userName);
-};
-sendGreeting();
-sendGreeting('Good morning');
-sendGreeting('good afternoon', 'nomi');
--- ts/app.ts
+++
@@ -1,8 +0,0 @@
-const sendGreeting = (message='hello', userName='there'):void => 
-    console.log(`${message}, ${userName}`);
-    
-
-
-sendGreeting();
-sendGreeting('Good morning');
-sendGreeting('good afternoon', 'nomi');
