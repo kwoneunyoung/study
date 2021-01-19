f492b26bdee6da318f2814d9f48135ba6f1e6467
kwoneunyoung <dmsdud4972@naver.com>
2021-01-19 15:31:22 +0900

    1

--- ts/learn-typescript-master/why-ts/sample.js
+++ ts/learn-typescript-master/why-ts/sample.js
@@ -1,4 +1,16 @@
+// function sum(a,b) {
+//     return a+b;
+// }
+// sum(10,'20');
+// var result = sum(10,20);
+// result.toLocaleString();
+
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
 function sum(a,b) {
     return a+b;
 }
-sum(10,'20');
