9e7c40163ed3d88d43019f5b694a83f6a6d87446
kwoneunyoung <dmsdud4972@naver.com>
2021-01-21 09:14:24 +0900

    1

--- ts/self/class-note/1_type-basic.ts
+++ ts/self/class-note/1_type-basic.ts
@@ -1,1 +1,26 @@
-var str='hello';
+    //js 문자열 선언
+    //var str='hello';
+
+    //ts 문자열 선언
+    const str: string='hello';
+    let num:number = 10;
+    let arr:Array<number>=[1,2,3];
+    let heroes:Array<string>=['nomi','fufu','eunsu'];
+    let items:number[] = [1,2,3];
+
+    //ts튜플(튜플 : 배열의 순서와 타입까지 정하는 것)
+    let address: [string, number] = ['wang',1];
+
+    //ts 객체
+    let obj:object = {};
+    // let person: object ={
+    //     name : 'capt',
+    //     age:100
+    // };
+    let person: {name:string, age:number} = {
+        name : 'fufu',
+        age : 6
+    }
+    //ts 진위값
+    let show:boolean = true;
+    
