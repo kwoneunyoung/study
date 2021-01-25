919c6d1814a97ae8d11d07a7551f73089ac72821
kwoneunyoung <dmsdud4972@naver.com>
2021-01-25 16:37:03 +0900

    1

--- ts/self/class-note/ts-moudules/app.ts
+++
@@ -1,6 +0,0 @@
-import {Todo} from './types'
-
-var item : Todo = {
-    title : '할 일1',
-    checked : false
-}
---
+++ ts/self/class-writing/1_type-basic.ts
@@ -0,0 +1,13 @@
+//js 문자열 선언방식
+//var str = 'hello';
+
+//ts 문자열 선언
+let str: string = 'hello';
+
+//ts 숫자 선언
+let num : number = 10;
+
+//ts 배열
+let arr: Array<number> = [1,2,3];
+let herose : Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1,2,3];
