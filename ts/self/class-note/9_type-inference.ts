0510494819e9b6505507499dfe02b5c65b26155d
kwoneunyoung <dmsdud4972@naver.com>
2021-01-25 10:13:12 +0900

    1

--- ts/self/class-note/9_type-inference.ts
+++ ts/self/class-note/9_type-inference.ts
@@ -1,6 +1,18 @@
+//타입 추론 기본
 var a = 10;
 
 function getB(b = 10) {
     var c = 'hi';
     return b + c;
+}
+10 + '10' //1010
+
+//타입 추론 기본2
+interface Dropdownn<T> {
+    value : T;
+    title : string;
+}
+var shoppingItem: Dropdownn<string> = {
+    value : 'abc',
+    title : 'hello'
 }
