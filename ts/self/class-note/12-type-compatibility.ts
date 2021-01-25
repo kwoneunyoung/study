dba8b79b0f0ca5bb36abd7543cca15720cda2196
kwoneunyoung <dmsdud4972@naver.com>
2021-01-25 13:45:30 +0900

    1

--- ts/self/class-note/12-type-compatibility.ts
+++ ts/self/class-note/12-type-compatibility.ts
@@ -4,17 +4,58 @@ interface Developers {
     skill: string;
 }
 
-// interface Jobs {
-//     name : string;
-//}
-
-class Jobs {
+interface Jobs {
     name : string;
-    skill : string;
+    //skill : string;
 }
+
+// class Jobs {
+//     name : string;
+//     skill : string;
+// }
 
 
 var developers : Developers;
 var jobs : Jobs;
 //developers = jobs;
-developers = new Jobs();
+//developers = jobs;
+
+//함수
+var plus = function(a:number) {
+}
+var som = function(a:number, b:number ) {
+    
+}
+som = plus;
+//plus = som;
+
+
+//제네릭
+interface Empty<T> {
+
+}
+var empty1:Empty<string>;
+var empty2:Empty<number>;
+//empty1 = empty2;
+//empty2 = empty1
+
+interface NotEmpty<T> {
+    data : T;
+
+}
+
+var notempty1 : NotEmpty<string>;
+var notempty2 : NotEmpty<number>;
+
+
+
+
+
+
+
+
+
+
+
+
+
