6e38e3988970d7f300d6075ff9ebf756ad84f2c1
kwoneunyoung <dmsdud4972@naver.com>
2021-01-26 13:45:33 +0900

    1

--- ts/self/class-writing/5_operator.ts
+++ ts/self/class-writing/5_operator.ts
@@ -5,9 +5,29 @@
 // logMessage(100);
 // logMessage(false);
 
+var sehoo : string | number | boolean;
 function logMessage(value:string | number) {
-    console.log(value);
-    
+    if(typeof value == 'number') {
+        value.toLocaleString();     
+    }
+    if(typeof value == 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number'); 
 }
 logMessage('hello');
 logMessage(100);
+
+
+interface Deverloper {
+    name : string;
+}
+
+interface Person {
+    name : string;
+    age:number;
+}
+
+function askSomeone(someone:Developer | Person) {
+
+}
