0b54b7d465fd56eb925c9a354028fa80257cb59d
kwoneunyoung <dmsdud4972@naver.com>
2021-01-26 09:51:35 +0900

    1

--- ts/self/class-writing/2_functions.ts
+++ ts/self/class-writing/2_functions.ts
@@ -10,7 +10,15 @@ function add() : number {
 }
 
 // 함수에 타입을 정의하는 방식
-function sum(a:number, b:number):number {
-    return a+b;
+// function sum(a:number, b:number):number {
+//     return a+b;
+// }
+// sum(10,20);
+
+//함수의 옵셔널 파라미터
+function logg(a:string, b?:string) {
+
 }
-sum(10,20);
+
+logg('hello world');
+logg('hello ts', 'abc');
