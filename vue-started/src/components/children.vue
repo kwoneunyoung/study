1f0becd025121cc1d30333322f22abaf43eb9c96
kwoneunyoung <dmsdud4972@naver.com>
2021-02-05 14:12:00 +0900

    1

---
+++ vue-started/src/components/children.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>{{parentMessage}}</div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class Children extends Vue {
+        @Prop() parentMessage? : string;
+    }
+</script>
--- vue-started/src/views/Home.vue
+++ vue-started/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <message></message>
+    <children :parentMessage ="message"></children>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import message from '@/components/message.vue';
+import children from '@/components/children.vue';
 @Component({
   components: {
-    message,
+    children,
     
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  message:string ='hello world';  
+}
 </script>
