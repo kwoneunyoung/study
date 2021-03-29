0ca0a67a9a3bc2bdab10cf3bdeb53f3ad3262a5f
kwoneunyoung <dmsdud4972@naver.com>
2021-03-29 22:18:21 +0900

    1

--- vue/todo/my-todo/src/components/TodoList.vue
+++ vue/todo/my-todo/src/components/TodoList.vue
@@ -1,9 +1,14 @@
 <template>
    <section>
        <ul>
-           <li>할 일 1</li>
-           <li>할 일 2</li>
-           <li>할 일 3</li>
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+        
+            <i class="fa fa-trash-o" aria-hidden="true"></i>
+        </span>
+        </li>
        </ul>
    </section>
 </template>
@@ -13,6 +18,11 @@
         data() {
             return {
                 todoItems: []
+            }
+        },
+        methods: {
+            removeTodo() {
+                cosole.log('clicked');
             }
         },
         created() {
@@ -27,4 +37,29 @@
 </script>
 
 <style>
+    ul {
+        list-stlye-type:none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: center;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
