eb9113beeda8cadf4ffa2b8a905eb07da291047e
kwoneunyoung <dmsdud4972@naver.com>
2021-03-31 20:35:37 +0900

    로컬스토리지 created

--- vue/re-todo/re-todo/src/components/TodoList.vue
+++ vue/re-todo/re-todo/src/components/TodoList.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul>
+            <li>ㅎㅇ</li>
+        </ul>
+    </section>
 </template>
 
 <script>
     export default {
+        data() {
+            return{
+            todoItems : []
+            }
+        },
+        created() {
+            if(localStorage.length > 0 ) {
+                for(var i=0; i<localStorage.length; i++) {
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
 
     }
 </script>
