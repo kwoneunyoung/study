24e0b3bb001fe1007468b67a3bfe1270a964b3f9
kwoneunyoung <dmsdud4972@naver.com>
2021-03-31 21:26:03 +0900

    자바 기초-스캐너

---
+++ java/data structure/Lesson01/src/Code02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Code02 {
+
+	public static void main(String[] args) {
+
+		int number = 123;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter an integer : "  );
+		
+		int input = keyboard.nextInt();
+		if(input == number) {
+			System.out.println("Numbers match!");
+		}
+		else {
+			System.out.println("Numbers do not match!");
+		}
+	}
+
+}
