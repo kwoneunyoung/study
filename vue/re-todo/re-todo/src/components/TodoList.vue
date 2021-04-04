764efe9c40f4b91ad250f33777eb5cb40c6967c7
kwoneunyoung <dmsdud4972@naver.com>
2021-04-04 15:40:19 +0900

    버튼css 완

--- vue/re-todo/re-todo/src/components/TodoList.vue
+++ vue/re-todo/re-todo/src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
     <section>
         <ul>
-            <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+                <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+                {{todoItem}}</li>
+            <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)">
+                <i class="fa fa-trash-o" aria-hidden="true"></i>
+            </span>
         </ul>
     </section>
 </template>
@@ -25,4 +30,32 @@
 </script>
 
 <style>
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-top: 5px;
+    }
+    
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
