ad8ae5a5ef2c721eac2a70f0dd3b5d2abfd43c4a
kwoneunyoung <dmsdud4972@naver.com>
2021-04-04 16:05:22 +0900

    removeItme API로 할일 삭제

--- vue/re-todo/re-todo/src/components/TodoList.vue
+++ vue/re-todo/re-todo/src/components/TodoList.vue
@@ -1,14 +1,13 @@
 <template>
     <section>
         <ul>
-            <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
                 <i class="checkBtn fa fa-check" aria-hidden="true"></i>
                 {{todoItem}}
-            <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)">
-            <span class="removeBtn" type="button" @click="removeTodo"></span>
-                <i class="fa fa-trash-o" aria-hidden="true"></i>
-            </span>
-           </li> 
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)">
+                <i class="fa fa-trash-o" aria-hidden="true"></i>            
+                </span>
+            </li> 
         </ul>
     </section>
 </template>
@@ -29,8 +28,9 @@
         },
         methods : {
             
-            removeTodo() {
-                console.log('클릭햇움');
+            removeTodo(todoItem, index) {
+                localStorage.removeItem(todoItem);
+                this.todoItems.splice(index,1);
             }
         }
 
