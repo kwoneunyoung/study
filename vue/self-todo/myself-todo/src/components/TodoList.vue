20bdee3c4e3c300f87cb1ab04558ece5a1a59c96
kwoneunyoung <dmsdud4972@naver.com>
2021-04-08 20:36:20 +0900

    로컬스토리지 데이터를 뷰 데이터에 저장

--- vue/self-todo/myself-todo/src/components/TodoList.vue
+++ vue/self-todo/myself-todo/src/components/TodoList.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </section>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                todoItems : []
+            }
+        },
+        created() {
+            if(localStorage.length > 0) {
+                for(var i=0; i<localStorage.length; i++) {
+                    this.todoItems.push(localStorage.key(i));
+                }       
+            }
+        }
 
     }
 </script>
