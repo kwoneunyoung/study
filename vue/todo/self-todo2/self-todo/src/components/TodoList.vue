d292c277e0f3449b0d652a73cdc5ef2fd2b74386
kwoneunyoung <dmsdud4972@naver.com>
2021-04-12 19:28:45 +0900

    v-for 디렉티브 구현

--- vue/todo/self-todo2/self-todo/src/components/TodoList.vue
+++ vue/todo/self-todo2/self-todo/src/components/TodoList.vue
@@ -1,13 +1,26 @@
 <template>
     <section>
         <ul>
-            <li></li>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
         </ul>
     </section>
 </template>
 
 <script>
     export default {
+        data() {
+            return  {
+                todoItems : []
+            }
+        },
+
+        created() {
+            if(localStorage.length>0) { //로컬스토리지에 데이터가 있다면?
+                for(var i=0; i<localStorage.length; i++) {
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
        
     
     }
