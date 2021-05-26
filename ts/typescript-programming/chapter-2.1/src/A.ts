1ce0daa3712956d9f730e5585aa1d45a819fba6d
kwoneunyoung <dmsdud4972@naver.com>
2021-05-26 13:57:53 +0900

    methods

---
+++ ts/typescript-programming/chapter-2.1/src/A.ts
@@ -0,0 +1,8 @@
+export class A {
+    value : number = 1
+    method:() => void = function() : void {
+        console.log('value:${this.value}')
+
+    }
+
+}
--- ts/typescript-programming/chapter-2.1/src/index.ts
+++ ts/typescript-programming/chapter-2.1/src/index.ts
@@ -7,3 +7,4 @@ const testMakePerson = ():void => {
 }
 
 testMakePerson()
+
